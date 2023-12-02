c00df2274f1ed5a672d33718151862d39cd26016
shristikoju1 <57671396+shristikoju1@users.noreply.github.com>
2023-12-02 09:33:54 +0545

    Add files via upload

---
+++ day14/first.js
@@ -0,0 +1,51 @@
+console.log("Shristi");
+console.log("I love JS");
+
+name="Shristi Koju";
+console.log("name");
+console.log(name);
+age=22;
+console.log(age);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isFollow=true;
+console.log(isFollow);
+
+let address="Bhaktapur"
+let totalPrice = 1000;
+let rollNo=44;
+
+const student = {
+    fullName : "Suyan Shrestha",
+    age : 20,
+    isPass : true,
+};
+console.log(student.age);
+console.log(student["age"]);
+
+student["fullName"]="Sachin Tendulkar";
+console.log(student.fullName);
+
+// const brand = "nike";
+// brand = "addidas";
+
+const product ={
+    productName : "ParkerJotter Standard CT Ball Pen (Black)",
+    rating : 7.002,
+    offer : "5% off",
+    price : 270,
+};
+console.log(product);
+
+const profile ={
+    profileName : "Shristi Koju",
+    isFollow : true,
+    posts : 195,
+    followers : 500,
+    following : 300,
+};
+
+console.log(typeof profile["profileName"]);
+console.log(typeof profile["isFollow"]);
