084e1b52e5cab903f15291568cd4e7b439732922
shristikoju1 <57671396+shristikoju1@users.noreply.github.com>
2023-12-09 09:07:04 +0545

    Add files via upload

---
+++ day21/DOM/script.js
@@ -0,0 +1,27 @@
+document.getElementById("myId");
+console.dir(myId);
+
+
+let headings = document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.log(parahs);
+
+let elements = document.querySelector("p");         //first element
+console.dir(elements);
+
+let allElements = document.querySelectorAll("p");           //all element
+console.dir(allElements);
+
+let myButton = document.querySelector("#myId");         //first element
+console.dir(myButton);
+
+console.dir(document.body.firstChild);
+
+let myDiv = document.querySelector("div");
+console.dir(myDiv);
+
+
+
---
+++ day21/practice/script.js
@@ -0,0 +1,8 @@
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + "from Apna College Students";    
+
+
+let divs = document.querySelectorAll(".box");
+console.log(divs);
